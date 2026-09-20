1f6802f77f21163e68364e7e6ff229d46b159b74
pacew-webhook <alamremix@gmail.com>
2026-09-20 21:36:48 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,23 +11,21 @@ android {
         applicationId = "com.example.nataliacamo"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 40
+        versionName = "0.4"
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-}
 
-kotlin {
-    jvmToolchain(17)
+    kotlin { jvmToolchain(17) }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("com.google.android.material:material:1.12.0")
 }
