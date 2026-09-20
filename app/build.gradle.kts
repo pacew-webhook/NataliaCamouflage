9cb6045e6e0a8aa039b0d905e285d0efb7e31846
pacew-webhook <alamremix@gmail.com>
2026-09-21 00:27:16 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.nataliacamo"
         minSdk = 26
         targetSdk = 35
-        versionCode = 40
-        versionName = "0.4"
+        versionCode = 41
+        versionName = "0.5"
     }
 
     compileOptions {
@@ -28,4 +28,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("org.tensorflow:tensorflow-lite:2.14.0")
 }
