3be778c4be6f411f7144a2090f575b155008370c
pacew-webhook <alamremix@gmail.com>
2026-09-21 19:22:02 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.example.nataliacamo"
         minSdk = 26
         targetSdk = 35
-        versionCode = 51
-        versionName = "3.1.0"
+        versionCode = 52
+        versionName = "3.2.0"
     }
 
     compileOptions {
